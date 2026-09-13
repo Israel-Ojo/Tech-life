827c225f325dfefdb193821e37e3b4b4204937fb
Israel-Ojo <148692203+Israel-Ojo@users.noreply.github.com>
2026-09-13 06:27:19 +0100

    Delete darkmode.js

--- darkmode.js
+++
@@ -1,6 +0,0 @@
-// Check and apply theme from localStorage
- window.addEventListener('DOMContentLoaded', () => {
-      if (localStorage.getItem('theme') === 'dark') {
-        document.body.classList.add('dark');
-      }
-    });
